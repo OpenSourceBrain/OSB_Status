926705ca95ea02ed20205bf0b2af2ff8e080886a
adrianq <adrian.perez@ucl.ac.uk>
2014-04-04 15:13:39 +0100

    First iteration for a web and api test framework
    adrianq/OpenSourceBrain/redmine#92

---
+++ web/generalfunctions.py
@@ -0,0 +1,37 @@
+import urllib
+import urllib2
+import base64
+from bs4 import BeautifulSoup
+import smtplib
+
+USERNAME = None
+PASSWORD = None
+
+def get_page(url,username=None,password=None):
+    request = urllib2.Request(url)
+    if username is None:
+        username = USERNAME
+    if password is None:
+        password = PASSWORD
+    
+    result = None
+    req = urllib2.Request(url)
+    if username and password:
+        auth = base64.urlsafe_b64encode("%s:%s" % (username, password))
+        req.add_header("Authorization", "Basic %s" % auth)
+        #req.add_header("Content-Type", "application/json")
+        #req.add_header("Accept", "application/json")
+    try:
+        response = urllib2.urlopen(req)
+    except urllib2.HTTPError as e:
+        return None, e
+    else:
+        result = response.read()
+    return result, None
+
+    
+def sendmail(from_who, to, msg):
+    s = smtplib.SMTP('localhost')
+    s.sendmail(from_who, [to], msg)
+    s.quit()
+    
---
+++ web/test_api.py
@@ -0,0 +1,63 @@
+import random
+import unittest
+import sys
+from bs4 import BeautifulSoup
+import generalfunctions
+
+USERNAME = None
+PASSWORD = None
+
+url_base = "http://opensourcebrain.org/"
+
+url_path = {}
+#OSB Explore
+url_path["test_projects"] = "projects.json"
+
+
+#ERRORS
+CONTENT_PAGE_ERROR = "Content Page Error"
+
+class TestAPI(unittest.TestCase):
+
+    longMessage = True
+
+    @classmethod
+    def setUpClass(cls):
+        generalfunctions.USERNAME = USERNAME
+        generalfunctions.PASSWORD = PASSWORD  
+
+    def test_projects(self):
+        soup = self.check_general_aspects()
+        #CHECKING SPECIFIC ASPECTS
+        #self.assertIsNotNone(soup.find(id="osbcircle"), CONTENT_PAGE_ERROR)
+     
+        
+    def check_general_aspects(self, page_title='Open Source Brain'):
+        url = url_base + url_path[self._testMethodName]
+        page, e = generalfunctions.get_page(url)
+        if e is not None:
+            exceptionMessage = "URL: %s produced error %d (%s)" % (url,e.code,e.msg)
+            self.fail(exceptionMessage)
+            
+        self.assertIsNotNone(page, CONTENT_PAGE_ERROR + " / Page is blank")
+        soup = BeautifulSoup(page)
+        return soup
+
+if __name__ == '__main__':
+    for arg in sys.argv[1:]:
+        try:
+            key,value = arg.split(":")
+            if key == "username":            
+                USERNAME = value
+            if key == "password":
+                PASSWORD = value
+        except ValueError,e:
+            ignored_arg = arg
+            #print "Command line argument %s had error %s" % (arg,e.strerror)
+    
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestAPI)
+    testResult = unittest.TextTestRunner(verbosity=2).run(suite)  
+    
+    #if testResult.wasSuccessful() is False:
+        #mailText = "Errors\n" + str(testResult.errors) + "Failures\n" + str(testResult.failures)  
+        #generalfunctions.sendmail('adrianquintana@gmail.com', 'adrianquintana@gmail.com', mailText)
